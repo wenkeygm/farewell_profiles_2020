1bfe7e006fa4f9327347792afe0907971882e0e4
muni venkatesh ganji <59051745+rm-rf-error@users.noreply.github.com>
2020-04-03 15:52:34 +0530

    initalize

---
+++ js/script.js
@@ -0,0 +1,88 @@
+// event listener to respond to "Show another quote" button clicks
+// when user clicks anywhere on the button, the "printQuote" function is called
+//document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+var message = '';
+var red;
+var green;
+var blue;
+var viewedQuotes =[];
+
+//Array to hold quotes, sources, citaitons & years
+var quotes = [
+	{
+		quote: "The only thing to fear is fear itself.",
+		source: "Franklin Delano Roosevelt",
+		citation: "First Inaugural Address",
+		year: 1932,
+		tags: "Business"
+	},
+	{
+		quote: "That's one small step for man, one giant leap for mankind.",
+		source: "Neil Armstrong",
+		citation: "The moon",
+		year: 1969,
+		tags: "Space Travel",
+	},
+	{
+		quote: "It always seems impossible until it is done.",
+		source: "Nelson Mandela",
+		citation: "",
+		year: "1918 - 2013",
+		tags: "Politics",
+	},
+	{
+		quote: "Not everything that can be counted counts, and not everything that counts can be counted.",
+		source: "Albert Einstein",
+		citation: "",
+		year: "1879 - 1955",
+		tags: "Engineering",
+	},
+	{
+		quote: "What we think, we become",
+		source: "Buddha",
+		citation: "",
+		year: "",
+		tags: "Faith",
+	},
+	{
+		quote: "Dream big and dare to fail",
+		source: "Norman Vaughan",
+		citation: "",
+		year: "1905 - 2005",
+		tags: "Business",
+	},
+];
+
+function print(quote) {
+	var outputDiv = document.getElementById('quote-box');
+	outputDiv.innerHTML = quote;
+}
+// Function to get a random object from quotes array and store in variable randomQuote
+function getRandomQuote() {
+	var randomQuote = Math.floor(Math.random() * quotes.length);
+
+	var splicedQuote = quotes.splice(randomQuote, 1)[0];
+	viewedQuotes.push(splicedQuote);
+	if ( quotes.length === 0 ) {
+		quotes = viewedQuotes;
+		viewedQuotes = [];
+	}
+	return splicedQuote;
+}
+
+
+// Function to take previously selected random object from array & print to screen
+function printQuote() {
+	// On click event, function printQuote runs, then fires the getRandomQuote function
+	var quotes = getRandomQuote();
+	message ='<p class="quote">' + quotes.quote + '</p>';
+	message  += '<p class="source">' + quotes.source;
+	if ( quotes.citation ) {
+		message += '<span class="citation">' + quotes.citation + '</span>';
+	} else {
+		message += '';
+	}
+	print(message);
+	//Generate random color
+
+}
